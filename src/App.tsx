1d07f5a6e0be22544e9fc9bb9a535e5309603f43
pvt12 <pvt12@gcloud.ua.es>
2025-01-26 11:06:25 +0100

    Funcionalidad sin estilos

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { ChangeEvent, useState } from "react";
+import "./App.css";
+import EnrolmentForm from "./components/EnrolmentForm";
+
+function App() {
+  const [ugEnrolments, setUGEnrolments] = useState(0);
+  const [program, setProgram] = useState("UG"); // Para el select
+  const [pgEnrolments, setPGEnrolments] = useState(0);
+  const [enrolments, setEnrolments] = useState(0); // Variable para contabilizar el numero de matriculas
+
+  // Funcion para cambiar en el SELECT
+  const handleChangeProgram = (event: ChangeEvent<HTMLLIElement>) => {
+      setProgram(event.target.value.toString());
+ };
+
+  // Evento que se comunica con el hijo
+  const handleChangeEnrolments = (updateEnrolments: number) => {
+    setEnrolments(updateEnrolments);
+  };
+
+  const selectedEnrolments = (): number => {
+    return program == "UG" ? ugEnrolments : pgEnrolments;
+  };
+
+  return (
+    <>
+      <div className="App">
+        <div className="programs">
+          <ul className="ulEnrol">
+          <label>Selecciona el tipo de estudio:</label>
+            <li className="parentLabels" onChange={handleChangeProgram}>
+              <input
+                type="radio"
+                value="UG"
+                name="programGroup"
+                defaultChecked
+              />
+              Grado
+              <input
+                type="radio"
+                className="radioSel"
+                value="PG"
+                name="programGroup"
+              />
+              Postgrado
+            </li>
+            <li>Matriculaciones actuales: {enrolments}</li>
+          </ul>
+        </div>
+        <EnrolmentForm
+          chosenProgram={program}
+          onChangeEnrolments={handleChangeEnrolments}
+          currentEnrolments={enrolments}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
